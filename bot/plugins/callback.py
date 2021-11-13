db344da0f9da8e06924513c574e9365270c6da39
DevAXD <93697755+DevAXD@users.noreply.github.com>
2021-11-13 11:44:36 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -580,7 +580,7 @@ async def cb_disconnect(bot, update: CallbackQuery):
     remove_active = await db.del_active(chat_id, int(channel_id))
     
     if not remove_active:
-        await update.answer("Couldnt Full Fill YOur Request...\n Report This @CrazyBotszGrp Along With Bot's Log", show_alert=True)
+        await update.answer("Couldnt Full Fill YOur Request...\n Report This to support group  With Bot's Log", show_alert=True)
         return
     
     text= f"<i>Sucessfully Disconnected From</i> <code>{channel_name}</code>\n"
@@ -1601,14 +1601,14 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Filter-Bot-V2">Source</a>"""
+    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://github.com/DevAXD/fiLTER">support group</a>\n"""
+    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/DevAXD/fiLTER">Source</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/rosebakthan"
+                    "My Dev ⚡", url="https://github.com/DevAXD"
                 ),
                 
             InlineKeyboardButton
@@ -1640,12 +1640,15 @@ async def callback_data(bot, update: CallbackQuery):
     if query_data == "start":
         buttons = [[
             InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/rosebakthan'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/kidiloskahyper45')
+            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/DevAXD/fiLTER')
         ],[
             InlineKeyboardButton('Support 🛠', url='https://t.me/rosebakthan')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
+        ],[
+            InlineKeyboardButton('Subscribe 🛎', url='https://t.me/world_wide_movies')
         ]]
+        
     
         reply_markup = InlineKeyboardMarkup(buttons)
         
