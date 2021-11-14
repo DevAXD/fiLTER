22842e421ab4251a743e811e5a036e4d045d42e5
DevAXD <93697755+DevAXD@users.noreply.github.com>
2021-11-14 12:13:33 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,13 +1,13 @@
 import pymongo
 
-from info import DATABASE_URI, DATABASE_NAME
+from info import DATABASE_URI 
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
-mydb = myclient[DATABASE_NAME]
+mydb = ["DevAXD"]
 mycol = mydb['CONNECTION']   
 
 
